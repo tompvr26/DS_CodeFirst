0e9e134ce8bcc7426b616a4e722a3962e246cd2e
1ertest <tom.poyvre@epsi.fr>
2019-11-14 15:21:46 +0100

    second commit

---
+++ DSCodeFirst/DSCodeFirst.ORM/Jeux.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSCodeFirst.ORM
+{
+    public class Jeux
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Nom { get; set; }
+
+        [StringLength(50)]
+        public string Plateforme { get; set; }
+    }
+}
